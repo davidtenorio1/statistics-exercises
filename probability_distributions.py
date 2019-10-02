0163424f976108c6dd943e675198e3fe3f540277
davidtenorio1 <dtenorio2169@gmail.com>
2019-10-02 15:49:11 -0500

    new exercises

---
+++ probability_distributions.py
@@ -0,0 +1,83 @@
+from scipy import stats
+from scipy.stats import norm, binom
+
+# 1.) A bank found that the average number of cars waiting during the noon hour at a drive-up window follows a Poisson distribution with a mean of 2 cars. Make a chart of this distribution and answer these questions concerning the probability of cars waiting at the drive-up window.
+cars_distribution = stats.poisson(2)
+
+# What is the probability that no cars drive up in the noon hour?
+cars_distribution.cdf(0) #.135
+
+# What is the probability that 3 or more cars come through the drive through?
+cars_distribution.sf(2) #.323
+
+# How likely is it that the drive through gets at least 1 car?
+cars_distribution.sf(0) #.865
+
+
+
+
+
+
+# 2.) Grades of State University graduates are normally distributed with a mean of 3.0 and a standard deviation of .3. Calculate the following:
+# What grade point average is required to be in the top 5% of the graduating class?
+gpa_distribution = norm(3, .3)
+gpa_distribution.isf(.05) #3.493
+
+# An eccentric alumnus left scholarship money for students in the third decile from the bottom of their class. Determine the range of the third decile. 
+# Would a student with a 2.8 grade point average qualify for this scholarship?
+gpa_distribution.ppf(.3) #2.843
+
+
+
+
+
+
+# 3.) A marketing website has an average click-through rate of 2%. One day they observe 4326 visitors and 97 click-throughs. How likely is it that this many people or more click through?
+click_through_distribution = binom(4326, .02)
+click_through_distribution.sf(96) #.140
+
+
+
+
+
+# 4.) You are working on some statistics homework consisting of 100 questions where all of the answers are a probability rounded to the hundreths place. 
+# Looking to save time, you put down random probabilities as the answer to each question.
+# What is the probability that at least one of your first 60 answers is correct?
+homework_distribution = binom(60,.01)
+homework_distribution.sf(0) #.453
+
+
+
+
+
+# 5.) The codeup staff tends to get upset when the student break area is not cleaned up. 
+# Suppose that there's a 3% chance that any one student cleans the break area when they visit it, and, on any given day, about 90% of the 3 active cohorts of 22 students visit the break area. 
+# How likely is it that the break area gets cleaned up each day?
+visit_distribution = binom(66, .9)
+avg_num_visits = visit_distribution.isf(.9)
+cleaned_distribution = binom(avg_num_visits, .03)
+cleaned_distribution.sf(.9) #.818
+
+ # How likely is it that it goes two days without getting cleaned up? 
+uncleaned_two_days = (1-(cleaned_distribution.sf(.9))) ** 2
+#.033
+
+ # All week?
+uncleaned_seven_days = (1-(cleaned_distribution.sf(.9))) ** 7
+# 6.52 * 10^-6
+
+
+
+# 6.) You want to get lunch at La Panaderia, but notice that the line is usually very long at lunchtime. 
+# After several weeks of careful observation, you notice that the average number of people in line when your lunch break starts is normally distributed with a mean of 15 and standard deviation of 3. 
+# If it takes 2 minutes for each person to order, and 10 minutes from ordering to getting your food, what is the likelihood that you have at least 15 minutes left to eat your food before you have to go back to class? 
+# Assume you have one hour for lunch, and ignore travel time to and from La Panaderia.
+
+
+
+
+
+
+
+
+
--- simulation.py
+++ simulation.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 
 # 1.) How likely is it that you roll doubles when rolling two dice?
-n_trials = 1_000_000
+n_trials = 10_000
 n_dice = 2
 rolls = np.random.choice([1, 2, 3, 4, 5, 6], n_trials * n_dice).reshape(n_trials, 2)
 df = pd.DataFrame(rolls)
@@ -12,14 +12,16 @@ df['rolled_doubles'] = df[0] == df[1]
 doubles_probability = df.rolled_doubles.mean()
 doubles_probability
 
-# 2.) If you flip 8 coins, what is the probability of getting exactly 3 heads? What is the probability of getting more than 3 heads?
-n_trials = 1_000_000
+# 2.) If you flip 8 coins, what is the probability of getting exactly 3 heads? 
+n_trials = 10_000
 n_flips = 8
 flips = np.random.choice([0, 1], n_trials * n_flips).reshape(n_trials, n_flips)
 df = pd.DataFrame(flips)
 df['three_heads'] = df.sum(axis=1) == 3
 exactly_three_probability = df.three_heads.mean()
 exactly_three_probability
+
+#What is the probability of getting more than 3 heads?
 df['more_than_three'] = df.sum(axis=1) > 3
 more_than_three_probability = df.more_than_three.mean()
 more_than_three_probability
@@ -27,7 +29,7 @@ more_than_three_probability
 # 3.) There are approximitely 3 web development cohorts for every 1 data science cohort at Codeup. 
 # Assuming that Codeup randomly selects an alumni to put on a billboard, what are the odds that the two billboards I drive past both have data science students on them?
 
-n_trials = 1_000_000
+n_trials = 10_000
 n_billboards = 2
 drives = np.random.choice(['web1', 'web2', 'web3', 'ds'], n_trials * n_billboards).reshape(n_trials, n_billboards)
 df = pd.DataFrame(drives)
@@ -38,7 +40,7 @@ ds_probability
 
 # 4.) Codeup students buy, on average, 3 poptart packages (+- 1.5) a day from the snack vending machine. 
 # If on monday the machine is restocked with 17 poptart packages, how likely is it that I will be able to buy some poptarts on Friday afternoon?
-pop_tarts = np.random.normal(3,1.5,(1_000_000,5))
+pop_tarts = np.random.normal(3,1.5, (10_000,5))
 df = pd.DataFrame(pop_tarts)
 df['sum'] = df.sum(axis=1)
 df['poptarts_left'] = df['sum'] < 17
@@ -49,15 +51,15 @@ poptarts_probability
 #Men have an average height of 178 cm and standard deviation of 8cm.
 #Women have a mean of 170, sd = 6cm.
 #If a man and woman are chosen at random, P(woman taller than man)?
-df_men = pd.DataFrame(np.random.normal(178,8,(1_000_000,1)))
-df_women = pd.DataFrame(np.random.normal(170,6,(1_000_000,1)))
+df_men = pd.DataFrame(np.random.normal(178,8, (10_000,1)))
+df_women = pd.DataFrame(np.random.normal(170,6, (10_000,1)))
 df_women['woman_taller_than_man'] = df_women > df_men
 taller_woman_probability = df_women.woman_taller_than_man.mean()
 taller_woman_probability
 
 # 6.) When installing anaconda on a student's computer, there's a 1 in 250 chance that the download is corrupted and the installation fails. 
 # What are the odds that after having 50 students download anaconda, no one has an installation issue? 
-fifty_data = np.random.random((1_000_000,50))
+fifty_data = np.random.random( (10_000,50))
 not_corrupted = fifty_data > (1/250)
 df_not_corrupted = pd.DataFrame(not_corrupted)
 df_not_corrupted['none_corrupted'] = df_not_corrupted.all(1)
@@ -65,7 +67,7 @@ none_corrupted_probability = df_not_corrupted.none_corrupted.mean()
 none_corrupted_probability
 
 # 100 students?
-hundred_data = np.random.random((1_000_000,100))
+hundred_data = np.random.random( (10_000,100))
 not_corrupted = hundred_data > (1/250)
 df_not_corrupted = pd.DataFrame(not_corrupted)
 df_not_corrupted['none_corrupted'] = df_not_corrupted.all(1)
@@ -73,7 +75,7 @@ none_corrupted_probability = df_not_corrupted.none_corrupted.mean()
 none_corrupted_probability
 
 # What is the probability that we observe an installation issue within the first 150 students that download anaconda?
-first_one_fifty_data = np.random.random((1_000_000,150))
+first_one_fifty_data = np.random.random( (10_000,150))
 corrupted = first_one_fifty_data < (1/250)
 df_corrupted = pd.DataFrame(corrupted)
 df_corrupted['at_least_one_corrupted'] = df_corrupted.any(1)
@@ -81,7 +83,7 @@ one_corrupted_probability = df_corrupted.at_least_one_corrupted.mean()
 one_corrupted_probability
 
 # How likely is it that 450 students all download anaconda without an issue?
-four_fifty_data = np.random.random((1_000_000,450))
+four_fifty_data = np.random.random( (10_000,450))
 lucky = four_fifty_data > (1/250)
 df_lucky = pd.DataFrame(lucky)
 df_lucky['none_corrupted'] = df_lucky.all(1)
@@ -90,14 +92,14 @@ no_corrupted_probability
 
 # 7.) There's a 70% chance on any given day that there will be at least one food truck at Travis Park. However, you haven't seen a food truck there in 3 days. How unlikely is this?
 # How likely is it that a food truck will show up sometime this week?
-data = np.random.random((1_000_000,3))
+data = np.random.random( (10_000,3))
 ft = data > .7
 df_ft = pd.DataFrame(ft)
-df_ft['food_truck_present'] = df_ft.any(1)
+df_ft['food_truck_present'] = df_ft.all(1)
 three_days_missing_probability = df_ft.food_truck_present.mean()
 three_days_missing_probability
 
-data2 = np.random.random((1_000_000,7))
+data2 = np.random.random( (10_000,7))
 ft_present = data2 <= .7
 df_ft = pd.DataFrame(ft_present)
 df_ft['food_truck_present'] = df_ft.any(1)
@@ -105,21 +107,21 @@ once_this_week_probability = df_ft.food_truck_present.mean()
 once_this_week_probability
 
 # 8.) If 23 people are in the same room, what are the odds that two of them share a birthday? 
-birthdays = np.random.randint(1,365,(1_000_000,23))
+birthdays = np.random.randint(1,365, (10_000,23))
 df_birthdays = pd.DataFrame(birthdays)
 df_birthdays['uniques'] = (df_birthdays.nunique(1))
 df_birthdays['same_birthdays'] = df_birthdays.uniques != 23
 same_probability = df_birthdays.same_birthdays.mean()
 
 # What if it's 20 people?
-birthdays = np.random.randint(1,365,(1_000_000,20))
+birthdays = np.random.randint(1,365, (10_000,20))
 df_birthdays = pd.DataFrame(birthdays)
 df_birthdays['uniques'] = (df_birthdays.nunique(1))
 df_birthdays['same_birthdays'] = df_birthdays.uniques != 20
 same_probability = df_birthdays.same_birthdays.mean()
 
 # What if it's 40 people?
-birthdays = np.random.randint(1,365,(1_000_000,40))
+birthdays = np.random.randint(1,365, (10_000,40))
 df_birthdays = pd.DataFrame(birthdays)
 df_birthdays['uniques'] = (df_birthdays.nunique(1))
 df_birthdays['same_birthdays'] = df_birthdays.uniques != 40
